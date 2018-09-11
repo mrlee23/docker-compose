f7871e30871174851b05bee6eee0c13c419290ca
Dongsoo Lee <dongsoolee@Dongsooui-MacBook-Pro.local>
2018-09-11 16:53:49 +0900

    Add xpressengine

---
+++ xpressengine/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -d xe ]
+then
+    mkdir xe
+    cd xe
+    wget https://github.com/xpressengine/xe-core/releases/download/1.9.8/xe.1.9.8.zip
+    unzip xe.1.9.8.zip && rm xe.1.9.8.zip
+fi
