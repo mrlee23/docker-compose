e745d4c243c550217915b13c2fb1788085f4862a
Dongsoo Lee <dongsoolee@Dongsooui-MacBook-Pro.local>
2018-09-11 16:59:14 +0900

    Update build.sh

--- xpressengine/build.sh
+++ xpressengine/build.sh
@@ -5,4 +5,6 @@ then
     cd xe
     wget https://github.com/xpressengine/xe-core/releases/download/1.9.8/xe.1.9.8.zip
     unzip xe.1.9.8.zip && rm xe.1.9.8.zip
+    mkdir files
+    chmod -R 707 files
 fi
